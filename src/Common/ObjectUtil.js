3a7cce911f7be34c6c93134f30b2dc456724854d
Simon <i@wuqian.me>
2017-12-08 15:17:42 +0800

    del arrayutil

--- src/Common/ObjectUtil.js
+++ src/Common/ObjectUtil.js
@@ -2,6 +2,52 @@
 
 export default class ObjectUtil {
 
+  static isEmpty(obj) {
+
+    if (typeof obj != 'object') {
+      return true;
+    }
+
+    for (let key in obj){
+      return false;
+    }
+
+    return true;
+  }
+
+  static index(obj, name) {
+    let indexedMap = {};
+
+    if (this.isEmpty(obj)) {
+      return indexedMap;
+    }
+
+    for (let item in obj) {
+      if (item.hasOwnProperty(name)) {
+        indexedMap[item[name]] = item;
+      }
+    }
+
+    return indexedMap;
+  }
+
+  static column(obj, columnName) {
+
+    let column = [];
+
+    if (this.isEmpty(obj)) {
+      return column;
+    }
+
+    for (let item in obj) {
+      if (item.hasOwnProperty(columnName)) {
+        column.push(item[columnName]);
+      }
+    }
+
+    return column;
+  }
+
   static requires(obj, keys) {
     for (let i = 0; i < keys.length; i++) {
       if (!Object.prototype.hasOwnProperty.call(obj, keys[i])) {
@@ -12,16 +58,13 @@ export default class ObjectUtil {
     return true;
   }
 
-  static isEmpty(obj) {
+  static parts(obj, keys) {
+    let parts = {};
 
-    if (typeof obj != 'object') {
-      return true;
+    for (let key of keys) {
+      parts[key] = obj[key];
     }
 
-    for (let key in obj){
-　　　　return false;
-　　 }
-
-　　 return true;
+    return parts;
   }
 }
